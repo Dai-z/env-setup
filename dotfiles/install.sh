a7bd7519722f29e0e0820b3634eca487c2440b68
Dai-z <xinxin.chen@tusimple.ai>
2020-12-16 13:10:42 +0800

    Use submodule for dotfile

--- dotfiles/install.sh
+++
@@ -1,6 +0,0 @@
-cp -avr ./.config/nvim/init.vim $HOME/ --parents
-cp -avr ./.oh-my-zsh/custom/themes $HOME/ --parents
-cp -avr ./.ssh/config $HOME/ --parents
-cp -avr ./.zshrc $HOME/ --parents
-cp -avr ./.tmux.conf $HOME/ --parents
-
--- dotfiles/update.sh
+++
@@ -1,6 +0,0 @@
-cp -avr $HOME/.config/nvim/init.vim  ./.config/nvim
-cp -avr $HOME/.oh-my-zsh/custom/themes ./.oh-my-zsh/custom
-cp -avr $HOME/.ssh/config ./.ssh/
-cp -avr $HOME/.zshrc ./
-cp -avr $HOME/.tmux.conf ./
-
